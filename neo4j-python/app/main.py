26eb278d12c78aaa1a1903bc3081ad7c63e89c2e
Bernese-Corgi <jnyng0621@gmail.com>
2023-03-18 17:01:37 +0900

    [neo4j-python] product route draft

--- neo4j-python/app/main.py
+++ neo4j-python/app/main.py
@@ -3,8 +3,9 @@ from typing import Union
 from fastapi import FastAPI
 from pydantic import BaseModel
 
-from app.router import people
+from app.router import people, product
 
 app = FastAPI()
 
 app.include_router(people.router, prefix='/people')
+app.include_router(product.router, prefix='/product')
